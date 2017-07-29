91fd894bb958daaccf973c064b1010666b7ebb5e
Evan Cole <master@Evans-MacBook-Pro.local>
2017-07-29 18:17:02 +0200

    final demo

--- README.md
+++
@@ -1,2 +0,0 @@
-version 7:  	
-  * dynamically draw board when page is loaded
--- tests.js
+++
@@ -1,3 +0,0 @@
-// no tests for this project, testing DOM manipulations is more advanced
-
-// you will be expected to have unit tests for pure JS assignments
